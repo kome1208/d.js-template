8e60e7522e19186d61c85dfaa8e29b8690bbe28f
komei <kome1208@outlook.jp>
2022-06-30 21:10:23 +0900

    すげー

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 const eventsPath = path.join(__dirname, 'events');
