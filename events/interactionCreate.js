3c95d53b6aee1be0d8c38a3e75dd7b2eae022cd7
82amfiiiii <101552871+82amfiiiii@users.noreply.github.com>
2022-08-10 14:04:11 +0900

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'interactionCreate',
 	execute(interaction, client) {
 		if (interaction.isCommand()) return;
-  	const command = client.commands.get(interaction.commandName);
+  	const command = client.slashcommands.get(interaction.commandName);
 	  if (!command) return;
   	try {
 	  	await command.execute(interaction);
