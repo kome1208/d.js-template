cd443f7f522a0c30ad22c0aaa0b393059a961816
komei <kome1208@outlook.jp>
2022-08-10 13:53:00 +0900

    [fix]コマンドが同時反応するのを修正した

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
-const { token, prefix } = require('./config.json');
+const { token } = require('./config.json');
 
 const client = new Client(
 	{ intents: [
@@ -18,63 +18,32 @@ const client = new Client(
 	}
 	);
 
-client.commands = new Collection();
+client.slashcommands = new Collection();
 const slashcmdPath = path.join(__dirname, 'commands/slash');
 const slashcmdFiles = fs.readdirSync('./commands/slash').filter(file => file.endsWith('.js'));
-
 for (const file of slashcmdFiles) {
 	const filePath = path.join(slashcmdPath, file);
 	const command = require(filePath);
-	client.commands.set(command.data.name, command);
+	client.slashcommands.set(command.data.name, command);
 }
 
-const msgcmdFiles = fs.readdirSync('./commands/message').filter(file => file.endsWith('.js'));
-
+client.prefixcommands = new Collection()
+const msgcmdFiles = fs.readdirSync('./commands/prefix').filter(file => file.endsWith('.js'));
 for (const file of msgcmdFiles) {
-	const command = require(`./commands/message/${file}`);
-	client.commands.set(command.name, command);
+	const command = require(`./commands/prefix/${file}`);
+	client.prefixcommands.set(command.name, command);
 }
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+		client.once(event.name, (...args) => event.execute(...args, client));
 	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
-client.on('interactionCreate', async interaction => {
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-client.on('messageCreate', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
 
 client.login(token);
