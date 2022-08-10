3c753290ddb411123f043a769a9719bf181cfd97
komei <kome1208@outlook.jp>
2022-08-10 14:06:50 +0900

    [fix]書き忘れを直した

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,7 +7,7 @@ module.exports = {
         const command = args.shift().toLowerCase();
         if (!client.prefixcommands.has(command)) return;
         try {
-            client.commands.get(command).execute(message, args);
+            client.prefixcommands.get(command).execute(message, args);
         } catch (error) {
             console.error(error);
             message.reply('there was an error trying to execute that command!');
