b4c4ce4c4149950f766c4bfa1236c371fd4d114d
82amfiiiii <101552871+82amfiiiii@users.noreply.github.com>
2022-08-16 12:23:39 +0000

    [add]コメントを追加

--- deploy.js
+++ deploy.js
@@ -1,14 +1,19 @@
+//各種モジュールの定義
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { token, clientId, guildId } = require('./config.json');
 const fs = require('node:fs');
 
+//配列を作成
 const commands = [];
+//スラッシュコマンドのフォルダを読み込む
 const commandFiles = fs.readdirSync('./commands/slash').filter(file => file.endsWith('.js'));
 
 
 for (const file of commandFiles) {
+	//スラッシュコマンドのファイルを読み込み
 	const command = require(`./commands/slash/${file}`);
+	//配列にぶち込む
 	commands.push(command.data.toJSON());
 }
 
@@ -16,8 +21,8 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
 	try {
+		//コマンドの登録を開始
 		console.log('Started refreshing application (/) commands.');
-
 		await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
