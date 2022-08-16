1338668c19a2a4a0ed889002d64dc1383c5aa0b3
82amfiiiii <101552871+82amfiiiii@users.noreply.github.com>
2022-08-16 12:41:47 +0000

    [add]コメントを追加

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,16 +1,18 @@
+//prefixを読み込む
 const { prefix } = require('../config.json');
+
 module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-        if (!client.prefixcommands.has(command)) return;
-        try {
-            client.prefixcommands.get(command).execute(message, args);
-        } catch (error) {
-            console.error(error);
-            message.reply('there was an error trying to execute that command!');
+        if (!message.content.startsWith(prefix) || message.author.bot) return; //メッセージ内容がprefixから始まらない、もしくはユーザーがbotだったら処理を停止
+        const args = message.content.slice(prefix.length).trim().split(/ +/); //prefix以外の内容だけを取り出す
+        const command = args.shift().toLowerCase(); //コマンド名だけを取り出す
+        if (!client.prefixcommands.has(command)) return; //prefixコマンドリストに取り出したコマンド名が存在しなかったら処理を停止
+        try { //実行を試みる
+            client.prefixcommands.get(command).execute(message, args); //prefixコマンドのファイルを呼び出す
+        } catch (error) { //エラーが出たらキャッチして「error」に格納
+            console.error(error); //コンソールにエラー内容を出力
+            message.reply('there was an error trying to execute that command!'); //「エラーが発生した」と返信
         }
 	},
 };
