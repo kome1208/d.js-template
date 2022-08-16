6b198f4683f9f5c6f5f4082a1956b7df0d091b19
82amfiiiii <101552871+82amfiiiii@users.noreply.github.com>
2022-08-16 12:48:02 +0000

    [add]コメントを追加

--- commands/prefix/ping.js
+++ commands/prefix/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
+	name: 'ping',　//コマンド名
+	description: 'Ping!', //コマンドの説明
 	execute(message) {
-		message.channel.send('Pong.');
+		message.channel.send('Pong.'); //コマンドが送られたチャンネルに「Pong.」を送信
 	},
 };
